ba26c94fb53393a4a69655e8f4e72cefef0e22f4
Puneethnaik <puneethn250@gmail.com>
2018-08-03 00:26:23 +0530

    built the login/logout functionality.

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var path = require('path');
+var login = require('./routes/loginRoutes');
+var session = require('client-sessions');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -6,15 +9,46 @@ var io = require('socket.io')(http);
 
 //middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//sessions
+app.use(session({
+    cookieName : 'session',
+    secret : 'groupchat_secret'
+}));
+
+
+
+//mysql
+var mysql  = require('mysql');
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'groupchat'
+})
+
+connection.connect(function(err){
+    if(err){
+        console.log("something wrong. Could not connect with database");
+    }
+    else console.log("mysql successfully connected");
+})
+
+
 io.on('connection', function(socket){
+    messages = [];
     console.log("a user connected");
     socket.on('disconnect', function(){
-        console.log("user disconnected");
+        console.log("user disconnected" + messages);
     })
     socket.on('chat message', function(msg){
         console.log('msg : ' + msg);
+        messages.push(msg);
         io.emit('chat message', msg);
     })
 });
@@ -24,6 +58,38 @@ app.get("/", function(req, res){
     console.log("Hello realtime world");
     res.sendFile(__dirname + "/index.html");
 });
+
+//loginlogout entry points
+var router = express.Router();
+router.get('/', function(req, res){
+    res.json("Welcome to this groupchat web app.");
+    console.log("hello");
+});
+
+router.post('/register', login.register);
+router.post('/login', login.login);
+
+app.use('/api/loginlogout', router);
+
+app.get('/login', function(req, res){
+    res.render('login.ejs', {title : "hello please login", type : "good"})
+})
+
+app.get('/register', function(req, res){
+    res.render('register.ejs', {type : "good"});
+});
+
+app.get('/profile', function(req, res){
+    if(!req.session.user)res.redirect('/login');
+    else
+        res.render('dashboard.ejs', {title:'Dashboard', user : req.session.user});
+});
+app.get('/logout', function(req, res){
+    req.session.user = undefined;
+    res.redirect('/profile');
+});
+//end loginlogout entry points
+
 
 var port = 3000 | process.env.port;
 http.listen(port, function(req, res){
