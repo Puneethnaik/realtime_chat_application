dc63995d5a4656071831f5bcbb53643a79c73d84
Puneethnaik <puneethn250@gmail.com>
2018-08-02 22:02:13 +0530

    basic chat functionality

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 var express = require('express');
-var bodyParser= require('body-parser');
 var app = express();
+var http = require('http').Server(app);
+var bodyParser= require('body-parser');
+var io = require('socket.io')(http);
+
+//middleware
 app.use(bodyParser.json());
 app.use(express.static("public"));
+
+io.on('connection', function(socket){
+    console.log("a user connected");
+    socket.on('disconnect', function(){
+        console.log("user disconnected");
+    })
+    socket.on('chat message', function(msg){
+        console.log('msg : ' + msg);
+        io.emit('chat message', msg);
+    })
+});
+
+
 app.get("/", function(req, res){
     console.log("Hello realtime world");
     res.sendFile(__dirname + "/index.html");
 });
+
 var port = 3000 | process.env.port;
-app.listen(port, function(req, res){
+http.listen(port, function(req, res){
     console.log("Connected on port " + port);
 });
